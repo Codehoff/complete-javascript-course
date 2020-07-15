a74b84ac6473800ce3e7e1c5a5460287c9117d99
Enno Kirchhoff <enno.kirchhoff1911@gmail.com>
2020-07-15 12:18:10 +0200

    comments

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,20 @@
+/******
+Variables and data types
+*/
+
+var firstName = 'John';
+console.log(firstName)
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job)
+
+
+/**
+ Variable mutation and type coercion
+ */
